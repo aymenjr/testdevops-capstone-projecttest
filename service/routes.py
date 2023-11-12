354aacb4d20c1131877083590c0c4b70d700eed9
aymenjr <139633302+aymenjr@users.noreply.github.com>
2023-11-12 19:01:46 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -72,6 +72,8 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
+
+    
     return jsonify(account_list), status.HTTP_200_OK
 
 
@@ -90,6 +92,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
+    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -111,6 +114,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
+    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -129,6 +133,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
 
+    
     return "", status.HTTP_204_NO_CONTENT
 
 
